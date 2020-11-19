595d413e1d88f0098691942f04f3f5a058e83b8d
gabrieldev525 <gvictor525.gv@gmail.com>
2020-11-18 21:55:58 -0300

    Configurado express e os pacotes bases para renderização do front

---
+++ main.js
@@ -0,0 +1,20 @@
+// importações
+const express = require('express')
+const app = express()
+const path = require('path')
+const bodyParser = require('body-parser')
+
+// configurações
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'))
+
+// cria as rotas
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+// inicia o servidor
+const port = 3000
+app.listen(port, () => console.log(`app running at http://localhost:${port}`))
