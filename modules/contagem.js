7b569429a1dd022af2676841511c0c71c8f9e63a
Helder Junior <heldercmj@edu.unifor.br>
2020-11-18 22:39:48 -0300

    adicionado um algoritmo de contagem

---
+++ modules/contagem.js
@@ -0,0 +1,9 @@
+function contagem (numero) {
+    console.log(`O valor informado é: ${numero} <br><hr><br>`);
+
+    console.log(`A quantidade de números entre 1 e o valor informado é: ${numero}`);
+}
+
+module.exports = {
+    contagem
+}
