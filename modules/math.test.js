043e1636d95906e8c6a428586e682f2fb7d6cfd6
gabrieldev525 <gvictor525.gv@gmail.com>
2020-12-12 21:06:25 -0300

    Criado casos de teste unitário para o algoritmo do quick sort

--- modules/math.test.js
+++ modules/math.test.js
@@ -21,3 +21,18 @@ test('O somatorio de 5 e 10 é igual a 15', () =>{
 test('O somatório de 5, 10, 15 e 20 é igual a 50', () => {
   expect(math.sum([5, 10, 15, 20])).toBe(50)
 })
+
+// Unit test to Quick sort
+test('Ordenando lista de valores inteiros', () => {
+  const unordered_values = [5, 25, 7, 9, 45, -5, 36]
+  const ordered_values = [-5, 5, 7, 9, 25, 36, 45]
+
+  expect(math.quickSort(unordered_values)).toStrictEqual(ordered_values)
+})
+
+test('Ordenando lista de valores com string e inteiros', () => {
+  const unordered_values = [5, '25', 7, 9, '45', '-5', 36]
+  const ordered_values = [-5, 5, 7, 9, 25, 36, 45]
+
+  expect(math.quickSort(unordered_values)).toStrictEqual(ordered_values)
+})
